c95176bd29e8aa06ea2f2660f90d761d8757a6b9
Nir Shoval <nirshoval2@gmail.com>
2023-06-10 21:45:31 +0300

    Fixed delete notes

--- main.js
+++ main.js
@@ -18,17 +18,19 @@ function addTask() {
     const task = {
         detail: taskDetailBox.value,
         date: dueDateBox.value,
-        time: timeBox.value
+        time: timeBox.value,
+        id: tasks.length
     };
+
+
+    // display array of tasks
+    displayNewTask(task, tasks.length);
 
     // Add object to array
     tasks.push(task);
 
     // save to local storage
     saveToLocalStorage();
-
-    // display array of tasks
-    displayNewTasks(task, tasks.length);
 
     // Clear form's inputs
     taskDetailBox.value = "";
@@ -39,14 +41,24 @@ function addTask() {
     taskDetailBox.focus();
 }
 
+// Find task index in array from element id
+function findTaskIndex(id) {
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].id == id) {
+            return i;
+        }
+    }
+    return -1;
+}
+
 // Display new tasks in the container
-function displayNewTasks(newTask, totalTasks) {
+function displayNewTask(newTask, currentTasksLength) {
     const notesContainer = document.getElementById("notesContainer");
     const child = document.createElement("div");
     child.setAttribute("class", "note");
     const date = new Date(newTask.date);
     const formattedDate = date.toLocaleDateString("en-GB");
-    child.innerHTML = `<span id ="${totalTasks}" class="deleteIcon btn btn-danger" onclick="deleteItem(this)">X</span>
+    child.innerHTML = `<span id ="${currentTasksLength}" class="deleteIcon btn btn-danger" onclick="deleteTask(this)">X</span>
     <h6 class="scroll">Task Detail:<br> ${newTask.detail} </h6>
     <p>Due Date: ${formattedDate}
     Time: ${newTask.time} </p>`
@@ -54,14 +66,14 @@ function displayNewTasks(newTask, totalTasks) {
 }
 
 // Display Tasks
-function displayTasks() {
+function displayAllTasks() {
     const notesContainer = document.getElementById("notesContainer");
     let html = "";
     for (let i = 0; i < tasks.length; i++) {
         const date = new Date(tasks[i].date);
         const formattedDate = date.toLocaleDateString("en-GB");
         html += `<div class="note">
-        <span id ="${i}" class="deleteIcon btn btn-danger" onclick="deleteItem(this)">X</span>
+        <span id ="${i}" class="deleteIcon btn btn-danger" onclick="deleteTask(this)">X</span>
         <h6 class="scroll">Task Detail:<br> ${tasks[i].detail} </h6>
         <p>Due Date: ${formattedDate}
         Time: ${tasks[i].time} </p>
@@ -71,13 +83,16 @@ function displayTasks() {
 }
 
 // Delete note
-function deleteItem(element) {
-    const index = element.id;
-    const note = document.getElementById(index);
-    tasks.splice(index, 1);
+function deleteTask(element) {
+    const id = element.id;
+    const note = document.getElementById(id);
     note.parentElement.remove();
+    const index = findTaskIndex(id);
+    if (index < 0) {
+        return;
+    }
+    tasks.splice(index, 1);
     saveToLocalStorage();
-    displayNewTasks();
 }
 
 // Save to Local Storage
@@ -92,5 +107,5 @@ function loadFromLocalStorage() {
     if (strTasks != null && strTasks.length > 0) {
         tasks = JSON.parse(strTasks);
     }
-    displayTasks();
+    displayAllTasks();
 }
